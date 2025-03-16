3cca92d9242cf2f1fe10e8681aa6d31863f27996
monitranjan <monitranjan2050@gmail.com>
2025-03-16 13:02:02 +0530

    account service dispatch event related changes

--- accounts/src/main/java/com/eazybytes/accounts/command/aggregate/AccountsAggregate.java
+++ accounts/src/main/java/com/eazybytes/accounts/command/aggregate/AccountsAggregate.java
@@ -6,6 +6,7 @@ import com.eazybytes.accounts.command.UpdateAccountCommand;
 import com.eazybytes.accounts.command.event.AccountCreatedEvent;
 import com.eazybytes.accounts.command.event.AccountDeletedEvent;
 import com.eazybytes.accounts.command.event.AccountUpdatedEvent;
+import com.eazybytes.common.event.AccountDataChangedEvent;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
@@ -31,7 +32,11 @@ public class AccountsAggregate {
     public AccountsAggregate(CreateAccountCommand createCommand) {
         AccountCreatedEvent accountCreatedEvent = new AccountCreatedEvent();
         BeanUtils.copyProperties(createCommand, accountCreatedEvent);
-        AggregateLifecycle.apply(accountCreatedEvent);
+        AccountDataChangedEvent accountDataChangedEvent = new AccountDataChangedEvent();
+        BeanUtils.copyProperties(createCommand, accountDataChangedEvent);
+        AggregateLifecycle.apply(accountCreatedEvent).andThen(
+                () -> AggregateLifecycle.apply(accountDataChangedEvent)
+        );
     }
 
     @EventSourcingHandler
@@ -47,7 +52,11 @@ public class AccountsAggregate {
     public void handle(UpdateAccountCommand updateCommand) {
         AccountUpdatedEvent accountUpdatedEvent = new AccountUpdatedEvent();
         BeanUtils.copyProperties(updateCommand, accountUpdatedEvent);
-        AggregateLifecycle.apply(accountUpdatedEvent);
+        AccountDataChangedEvent accountDataChangedEvent = new AccountDataChangedEvent();
+        BeanUtils.copyProperties(updateCommand, accountDataChangedEvent);
+        AggregateLifecycle.apply(accountUpdatedEvent).andThen(
+                () -> AggregateLifecycle.apply(accountDataChangedEvent)
+        );
     }
 
     @EventSourcingHandler
